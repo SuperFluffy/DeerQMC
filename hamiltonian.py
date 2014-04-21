3835fea142d0cfc58eed6c962969a2170635c19a
Richard Janis Beckert <janis.beckert@gmail.com>
2014-04-21 23:49:54 +0200

    Big rework of how the potential was calculated - most likely breaking other stuff

--- hamiltonian.py
+++ hamiltonian.py
@@ -2,36 +2,10 @@
 This module contains all functions pertaining to the construction of the
 Hamiltonian describing the lattice.
 """
-from numpy import array, diag, eye, complex128, float64, kron, zeros
+from numpy import array, copy, diag, empty, eye, exp, complex128, float64, kron, size, zeros
 
 from numpy.random import choice
 from scipy.linalg import expm2
-
-def auxiliary_field(timeslices,lattice_sites,spins=None): #{{{
-    """
-    Returns the space-time lattice with randomly generated auxiliary
-    spins.
-
-    Parameters
-    ----------
-    timeslices : int
-        The number of timeslices.
-    lattice_sites : int
-        The number of sites on the lattice.
-    spins : list, optional
-        The set of values the auxiliary spins can take. If None (default),
-        then only Ising spins (i.e. {↑,↓} represented as the list [+1,-1])
-        are used.
-
-    Returns
-    -------
-    spacetime : ndarray, shape (timeslices×lattice_sites)
-    """
-    if spins == None:
-        spins = [-1,+1]
-    randarray = choice(spinsSample,size=N*L)
-    spacetime = randarray.reshape(L,N)
-    return spacetime #}}}
 
 def shift_matrix(size,offset=1,periodic=True,period=1.0,dtype=float64): #{{{
     """
@@ -45,9 +19,9 @@ def shift_matrix(size,offset=1,periodic=True,period=1.0,dtype=float64): #{{{
 
     Parameters
     ----------
-    size : int
+    size : integer
         The order n of a n×n square matrix.
-    offset : int, optional
+    offset : integer, optional
         The distance from the principal diagonal for the super- and
         subdiagonals, as measured on the counterdiagonal.
         If 1 (default), the superdiagonal U and subdiagonal L are created
@@ -129,7 +103,7 @@ def neighbour_hopping(x,y=1,z=1,distance=1,shift=0,periodic=True,period=1.0,dtyp
     with_shift = lambda dim: shift_matrix(dim,offset=shift,periodic=periodic,period=period,dtype=dtype)
 
     k_x = with_distance(x)
-    s_y = with_shift((y)
+    s_y = with_shift(y)
 
     k = kron(s_y,k_x)
 
@@ -193,7 +167,7 @@ def hopping_matrix(x,y=1,z=1,couplings=None,periodic=True,period=1.0,dtype=float
         d = min(x,y) - 1
         maximum_neighbours = (d**2 + 3*d)//2
         neighbours = len(couplings)
-        
+
         if neighbours > maximum_degree:
             error_message = """Couplings for a hopping degree of up to {0} found, \
                 but only a degree of {1} stays within the lattice (dimensions (x,y) \
@@ -212,70 +186,186 @@ def hopping_matrix(x,y=1,z=1,couplings=None,periodic=True,period=1.0,dtype=float
                 k += -t * neighbour_hopping(x,y=y,z=z,distance=distance,shift=shift,periodic=periodic,period=period,dtype=dtype)
     return k #}}}
 
-def potential_matrix(paramDict,C,M,dtype=float64): #{{{
-    L = paramDict['L']
-    N = paramDict['N']
-    lambda1_general    = paramDict['lambda1 general']
-    lambda2_general    = paramDict['lambda2 general']
-    lambda1_domainWall = paramDict['lambda1 domainWall']
-    lambda2_domainWall = paramDict['lambda2 domainWall']
-    spinUp = paramDict['spinUp']
-    spinDn = paramDict['spinDn']
-
-    spinUp_other = paramDict['spinUp_other']
-    spinDn_other = paramDict['spinDn_other']
-
-    spacetime_1 = auxiliary_field(L,N)
-    spacetime_2 = auxiliary_field(L,N)
-
-    lattice_general = paramDict['lattice general']
-    lattice_domainWall = paramDict['lattice domainWall']
-
-    V1  = lambda1_general    * array([diag(space) for space in (lattice_general * spacetime_1)],dtype=float64)
-    V1 += lambda1_domainWall * array([diag(space) for space in (lattice_domainWall * spacetime_1)],dtype=float64)
-
-    V2  = lambda2_general    * array([diag(space) for space in (lattice_general * spacetime_2)],dtype=float64)
-    V2 += lambda2_domainWall * array([diag(space) for space in (lattice_domainWall * spacetime_2)],dtype=float64)
-
-    expVs_up = array([expm2(spinUp*v1 + spinUp_other * v2 + C + M) for (v1,v2) in zip(V1,V2)])
-    expVs_dn = array([expm2(spinDn*v1 + spinDn_other * v2 + C - M) for (v1,v2) in zip(V1,V2)])
-
-    return spacetime_1,spacetime_2,expVs_up,expVs_dn
-# }}}
-
-def hamiltonian(paramDict,dtype=float64): #{{{
+def auxiliary_field(timeslices,lattice_sites,spin_species=None): #{{{
     """
-    This function constructs all the Hamiltonian describing the system from the
-    quantities stored in the parameter dictionary.
+    Returns a randomly generated configuration of Ising spins for the
+    space-time lattice of the decoupled electron-electron interaction.
+
+    Parameters
+    ----------
+    timeslices : integer
+        The number of timeslices.
+    lattice_sites : integer
+        The number of sites on the lattice.
+    spin_species : list, optional
+        The set of values the auxiliary spins can take. If None (default),
+        then only Ising spins (i.e. {↑,↓} represented as the list [+1,-1])
+        are used.
+
+    Returns
+    -------
+    spacetime_field : ndarray, shape (timeslices×lattice_sites)
     """
-    x = paramDict['edgeLength x']
-    y = paramDict['edgeLength y']
-    N = paramDict['N']
-    t = paramDict['t']
-    periodic = paramDict['periodic']
-    period = paramDict['period']
-    mu = paramDict['mu']
-    B = paramDict['B']
-    dtau = paramDict['dtau']
+    if spin_species is None:
+        spin_species = [-1,+1]
+    randarray = choice(spin_species,size=N*L)
+    spacetime = randarray.reshape(L,N)
+    return spacetime_field #}}}
 
-    lambda1_general = paramDict['lambda1 general']
-    lambda2_general = paramDict['lambda2 general']
+def possible_values(lambda_lattice,spin_species=None): #{{{
+    """
+    Returns an array containing all possible values the decoupled potential
+    term can take on each lattice site depending on the auxiliary Ising.
+    Note: at the moment this assumes an isotropic distribution of the λ
+    coupling parameters in time direction.
 
-    lambda1_domainWall = paramDict['lambda1 domainWall']
-    lambda2_domainWall = paramDict['lambda2 domainWall']
+    Parameters
+    -----------
+    lambda_lattice : ndarray, shape n
+        An one-dimensional array of shape n (the number of lattice sites), which
+        consists of the decoupling parameter λ_i on each lattice site.
+    spin_species : list, optional
+        The set of values the auxiliary spins can take. If None (default),
+        then only Ising spins (i.e. {↑,↓} represented as the list [+1,-1])
+        are used.
 
-    spinUp = paramDict['spinUp']
-    spinDn = paramDict['spinDn']
+    Returns
+    -------
+    a : ndarray, shape s×n, where s = len(spin_species)
+    """
 
-    spinUp_other = paramDict['spinUp_other']
-    spinDn_other = paramDict['spinDn_other']
+    if spin_species is None:
+        spin_species = [-1,+1]
+
+    a = empty((len(spin_species), lambda_lattice.size))
+
+    for i,s in enumerate(spin_species):
+        a[i] = s * lambda_lattice
+
+    return a #}}}
+
+def decoupled_potential(timeslices,decoupling_terms,spin_species=None,dtype=float64): #{{{
+    """
+    Returns a random configuration of the auxiliary Ising spins for each of the
+    terms entering the decoupled potential, as well as the set of all possible
+    values that the decoupled terms can take on each lattice site.
+
+    Parameters
+    ----------
+    timeslices : integer
+        The number of timeslices.
+    decoupling_terms : list of ndarrays
+        A list containing a one-dimensional array of shape n with λ_{k,i}
+        values for each of the k decoupling terms on every space-site with
+        index i, where n is the total number of lattice sites.
+    spin_species : list, optional
+        The set of values the auxiliary spins can take. If None (default),
+        then only Ising spins (i.e. {↑,↓} represented as the list [+1,-1])
+        are used.
+
+    Returns
+    -------
+    fields : ndarray, shape (d×l×n), where
+        d = number of decoupling terms
+        l = number of timeslices
+        n = number of lattice sites
+
+    values : ndarray, shape (d×s×n), where s = len(spin_species)
+    """
+
+    lattice_sites = size(decoupling_terms[0])
+    d = len(decoupling_terms)
+
+    fields = empty((d,timeslices,lattice_sites),dtype=dtype)
+    values = empty((d,len(spin_species),lattice_sites),dtype=dtype1)
+
+    for i,lambda_lattice in enumerate(decoupling_terms):
+        fields[i] = auxiliary_fields(timeslices,lattice_sites,spin_species)
+        values[i] = possible_values(lambda_lattice,spin_spcies)
+
+    return fields,values #}}}
+
+def hamiltonian(parameter_dictionary): #{{{
+    """
+    Glues together the construction of all terms appearing in the Hamiltonian,
+    i.e. the kinetic, chemical, magnetic terms k, c, and m, respectively; also
+    includes the radom generation of a configuration for each of the
+    auxiliary fields decoupling the potential term, and the possible values the
+    decoupled terms can take on each lattice site.
+
+    Parameters
+    ----------
+    parameter_dictionary : dictionary
+        The dictionary containing all parameters describing the simulation.
+
+    Returns
+    -------
+    k : ndarray, shape (n×n), where
+        n = number of lattice sites
+    c : ndarray, shape (n×n)
+    m : ndarray, shape (n×n)
+    fields : ndarray, shape (d×l×n), where
+        d = the number of discretization terms
+        l = the number of time slices
+    values : ndarray, shape (d×s×n), where
+        s = the number of spin species/channels in the original Hamiltonian
+    """
+    x = parameter_dictionary['x']
+    y = parameter_dictionary['y']
+    z = parameter_dictionary['z']
+
+    t = parameter_dictionary['t']
+    mu = parameter_dictionary['mu']
+    b = parameter_dictionary['b']
+
+    decoupling_terms = parameter_dictionary['decoupling terms']
+    periodic = parameter_dictionary['periodic']
+    period = parameter_dictionary['period']
+
+    spin_species = parameter_dictionary['spin species']
+
+    lattice_sites = x*y*z
+
+    dtype = decoupling_terms[0].dtype
 
     k = hopping_matrix(x=x,y=y,z=1,neighbour_couplings=t,periodic=periodic,period=period,dtype=dtype)
-    exp_k = expm2(-dtau*k)
+    c = (-1) *  mu * eye(lattice_sites,dtype=dtype)
+    m = (-1) *  b  * eye(lattice_sites,dtype=dtype)
 
-    C = (dtau*mu) * eye(N,dtype=dtype)
-    M = (dtau*B)  * eye(N,dtype=dtype)
+    fields,values = decoupled_potential(timeslices,decoupling_terms,spin_species)
 
-    spacetime_1,spacetime_2,expVs_up,expVs_dn = potential_matrix(paramDict,C,M,dtype=dtype)
+    return k, c, m, fields, values #}}}
 
-    return exp_k, spacetime_1, spacetime_2, expVs_up, expVs_dn #}}}
+def discretized_exponentials(dtau,k,c,m,values): #{{{
+    """
+    Returns a dictionary of the discretized exponentials of the constants
+    Hamiltonian terms, as well as the exponentials of all values the decoupled
+    potential terms can take.
+    """
+
+    exp_dictionary['k'] = expm2(-dtau * k)
+    exp_dictionary['c'] = expm2(-dtau * c)
+    exp_dictionary['m'] = expm2(-dtau * m)
+
+    exp_dictionary['values'] = exp(values)
+
+    return exp_dictionary #}}}
+
+# {{{ Stuff that was refactored away 
+    #V1  = lambda1_general    * array([diag(space) for space in (lattice_general * spacetime_1)],dtype=float64)
+    #V1 += lambda1_domainWall * array([diag(space) for space in (lattice_domainWall * spacetime_1)],dtype=float64)
+
+    #V2  = lambda2_general    * array([diag(space) for space in (lattice_general * spacetime_2)],dtype=float64)
+    #V2 += lambda2_domainWall * array([diag(space) for space in (lattice_domainWall * spacetime_2)],dtype=float64)
+
+    #expVs_up = array([expm2(spinUp*v1 + spinUp_other * v2 + C + M) for (v1,v2) in zip(V1,V2)])
+    #expVs_dn = array([expm2(spinDn*v1 + spinDn_other * v2 + C - M) for (v1,v2) in zip(V1,V2)])
+
+    #lambda1_general = paramDict['lambda1 general']
+    #lambda2_general = paramDict['lambda2 general']
+
+    #lambda1_domainWall = paramDict['lambda1 domainWall']
+    #lambda2_domainWall = paramDict['lambda2 domainWall']
+#}}}
+
+
