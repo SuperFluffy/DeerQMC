95328caa7d2233713c11314387f2f355e63b7fb7
Richard Janis Beckert <janis.beckert@gmail.com>
2014-04-25 19:36:14 +0200

    Cleanup and use of generic dtype

--- support.py
+++ support.py
@@ -1,4 +1,4 @@
-import numpy 
+from numpy import dot,eye,float64,packbits,unique,zeros
 
 from math import ceil
 from math_functions import *
@@ -16,27 +16,27 @@ def compress_array(A): # {{{
     unsigned integer with padding, like 01010000, by setting the appropriate
     bits.
     """
-    uniqueElements = numpy.unique(A)
+    uniqueElements = unique(A)
     if uniqueElements.size > 2:
         raise ValueError("Array contains more than two unique elements: {0}".format(', '.join(map(str, uniqueElements))))
     else:
         needed_uints = ceil(A.size/64)
-        B = numpy.zeros(needed_uints * 64,dtype='u1')
+        B = zeros(needed_uints * 64,dtype='u1')
         B[ A==uniqueElements[0] ] = 0 # Translate the two unique elements in the
         B[ A==uniqueElements[1] ] = 1 # argument to 0, 1.
-        D = numpy.packbits(B)
+        D = packbits(B)
     return D.view('u8'), {uniqueElements[0]: 0, uniqueElements[1]: 1} #}}}
 
-def multiply_slices_start(N,expK,expVs,order): # Multiplies “B_i”s in a given order from the head. {{{
-    B = numpy.eye(N,dtype=numpy.complex128)
+def multiply_slices_start(N,expK,expVs,order,dtype=float64): # Multiplies “B_i”s in a given order from the head. {{{
+    B = eye(N,dtype=dtype)
     for l in order:
-        B = numpy.dot(B,numpy.dot(expK,expVs[l]))
+        B = dot(B,dot(expK,expVs[l]))
     return B #}}}
 
-def multiply_slices_end(N,expK,expVs,order): # Multiplies “B_i”s in a given order from the tail. {{{
-    B = numpy.eye(N,dtype=numpy.complex128)
+def multiply_slices_end(N,expK,expVs,order,dtype=float64): # Multiplies “B_i”s in a given order from the tail. {{{
+    B = eye(N,dtype=dtype)
     for l in order:
-        B = numpy.dot(numpy.dot(expK,expVs[l]),B)
+        B = dot(dot(expK,expVs[l]),B)
     return B #}}}
 
 def check_flip(p,gamma=None): #{{{
